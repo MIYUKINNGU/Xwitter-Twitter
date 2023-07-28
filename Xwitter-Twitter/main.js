7a0aed5b3ba01ebc509ccaf2124f9ec2faaf0dff
MIYUKINNGU <111403555+MIYUKINNGU@users.noreply.github.com>
2023-07-28 20:46:03 +0900

    Update main.js

--- Xwitter-Twitter/main.js
+++ Xwitter-Twitter/main.js
@@ -5,8 +5,19 @@ function home() {
 }
 function favicon() {
     const e = document.querySelectorAll('link[rel="shortcut icon"]')
-    console.log(e)
     if (e.length == 0) return;
     e[0].href = `data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII=`
 }
-window.addEventListener("mousemove", ()=>{home();favicon()})
+function title0() {
+    const e = document.querySelectorAll('title')
+    if (e.length == 0) return;
+    if (e[0].innerText.slice(-4, e[0].innerText.length) != " / X") return;
+    e[0].innerText = `${e[0].innerText.slice(0, -4)} / Twitter`
+}
+function title1() {
+    const e = document.querySelectorAll('title')
+    if (e.length == 0) return;
+    if (e[0].innerText != "X") return;
+    e[0].innerText = `Twitter`
+}
+window.addEventListener("mousemove", ()=>{home();favicon();title0();title1();})
